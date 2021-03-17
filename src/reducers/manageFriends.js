c8d2de1ef20fb62b29e03127a6be8bfa35b3e111
rhday <54642787+rhday@users.noreply.github.com>
2021-03-17 17:07:04 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,27 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {
+    friends: [],
+}, action){
+    switch(action.type){
+
+        case "ADD_FRIEND":
+            return({
+                ...state, friends: [
+                    ...state.friends,
+                    action.friend
+                ]
+            }) 
+            
+        case "REMOVE_FRIEND":
+            const removeId = state.friends.findIndex(friend => friend.id === action.id);
+        return({
+            ...state, friends:[
+                ...state.friends.slice(0, removeId),
+                ...state.friends.slice(removeId + 1)
+            ]
+        })
+            
+        
+        default:
+        return state;    
+    }
 }
